0ea4d8af8b1f10cd4a298f4ed3c7ef0605cab339
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-14 16:52:25 -0700

    Remove two part joke option

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -2,14 +2,13 @@
 import React, { useEffect, useState } from 'react';
 import './Card.css';
 
-const Card = ({id, joke, setup, delivery, saveCard}) => {
+const Card = ({id, joke, saveCard}) => {
 
   return (
     <section className='joke-card'>
       <button className='heart-button' onClick={() => saveCard(id)}>heart</button>
       <article key={id}>
-        <h3>{joke || setup}</h3>
-        <h4>{delivery}</h4>
+        <h3>{joke}</h3>
       </article>
     </section>
   )
