eab41f5e23e47ddd8c7e4ad327ca993580bfd640
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-14 16:53:14 -0700

    Add method to save favorite

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,15 +1,16 @@
 
-import React, { Component } from 'react';
-import { getJoke } from '../apiCalls/apiCalls';
+import React, { Component, useEffect, useState } from 'react';
+import { getJoke, addNewFavorite } from "../apiCalls/apiCalls";
 import Card from '../Card/Card';
 import Container from '../Container/Container';
+import Saved from '../Saved/Saved';
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
       jokes: [],
-      favorites: [],
+      favorites: []
     };
   }
 
@@ -19,11 +20,10 @@ class Home extends Component {
   };
 
   saveCard = (id) => {
-    console.log('click', id)
     const newFavorite = this.state.jokes.find(joke => joke.id === id )
     if (!this.state.favorites.includes(newFavorite)) {
-      this.state.favorites.push(newFavorite);
-      console.log(this.state.favorites)
+      this.state.favorites.push(newFavorite)
+      addNewFavorite(newFavorite.id, newFavorite.joke)
     } else {
       return console.log("NO")
     }
@@ -34,10 +34,52 @@ class Home extends Component {
       <div>
         <h2>Home component</h2>
         <Container jokeSlips={this.state.jokes} saveCard={this.saveCard} />
-        <Container jokeSlips={this.state.favorites} />
       </div>
     );
   }
 }
 
+// function Home() {
+//   const [jokes, setJokes] = useState([])
+//   const [favorites, setFavorites] = useState([])
+//   const [error, setError] = useState('')
+
+//   const getJoke = async () => {
+//     const url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=3"
+//     setError('')
+
+//     try {
+//       const response = await fetch(url)
+//       const jokes = await response.json()
+//       setJokes(jokes)
+//       console.log(jokes)
+//     } catch(error) {
+//       setError(error.message)
+//     }
+//   }
+
+//   useEffect(() => {
+//     getJoke()
+//   }, [jokes])
+
+//   function saveCard(id) {
+//     console.log("click", id);
+//     const newFavorite = jokes.find((joke) => joke.id === id);
+//     if (!favorites.includes(newFavorite)) {
+//       setFavorites(newFavorite)
+//       // favorites.push(newFavorite);
+//       console.log(this.state.favorites);
+//     } else {
+//       return console.log("NO");
+//     }
+//   };
+
+//     return (
+//       <div>
+//         <h2>Home component</h2>
+//         <Container jokeSlips={jokes} saveCard={saveCard} />
+//         <Container jokeSlips={favorites} />
+//       </div>
+//     );
+// }
 export default Home;
