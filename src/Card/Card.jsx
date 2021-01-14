b1175b4c8a83d5168e2c50804d9b9ded0dbdb2c3
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-13 17:11:45 -0700

    Add favorite functionality to card

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -2,11 +2,11 @@
 import React, { useEffect, useState } from 'react';
 import './Card.css';
 
-const Card = ({id, joke, setup, delivery}) => {
+const Card = ({id, joke, setup, delivery, saveCard}) => {
 
   return (
     <section className="joke-card">
-      <button>heart</button>
+      <button onClick={() => saveCard(id)}>heart</button>
       <article key={id}>
         <h3>{joke || setup}</h3>
         <h4>{delivery}</h4>
