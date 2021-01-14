5109abb8a0531eb701fd4e413408f68d0fdbef21
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-13 19:11:05 -0700

    Add save card functionality to Home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,27 +5,38 @@ import Card from '../Card/Card';
 import Container from '../Container/Container';
 
 class Home extends Component {
-  constructor () {
-    super()
-    this.state={
+  constructor() {
+    super();
+    this.state = {
       jokes: [],
-      favorites: []
-    }
+      favorites: [],
+    };
   }
 
-  componentDidMount= async () => {
+  componentDidMount = async () => {
     const fetchedJoke = await getJoke();
     this.setState({ jokes: fetchedJoke.jokes });
-  }
+  };
 
+  saveCard = (id) => {
+    console.log('click', id)
+    const newFavorite = this.state.jokes.find(joke => joke.id === id )
+    if (!this.state.favorites.includes(newFavorite)) {
+      this.state.favorites.push(newFavorite);
+      console.log(this.state.favorites)
+    } else {
+      return console.log("NO")
+    }
+  };
 
   render() {
-    return(
+    return (
       <div>
         <h2>Home component</h2>
-        <Container jokeSlips={this.state.jokes} saveCard={saveCard} />
+        <Container jokeSlips={this.state.jokes} saveCard={this.saveCard} />
+        <Container jokeSlips={this.state.favorites} />
       </div>
-    )
+    );
   }
 }
 
