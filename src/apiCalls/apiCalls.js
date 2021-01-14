238d69090a9cc218eddf73c4e00d8b1ac30f2491
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-14 16:51:38 -0700

    Add api call to post data to express server

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,7 +1,7 @@
 
 export const getJoke = async () => {
   const response = await fetch(
-    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=3"
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=2"
   );
   if(response.status >= 200 && response.status <=299 ) {
     const jsonResponse = await response.json();
@@ -11,4 +11,32 @@ export const getJoke = async () => {
     );
     return response.status;
   }
+}
+
+export const getAllFavorites = async () => {
+  const response = await fetch('http://localhost:3001/api/v1/favorites');
+  if(response.status >= 200 && response.status <= 299 ) {
+    const jsonResponse = response.json();
+    return jsonResponse;
+  } else {
+    console.log(`Error code ${response.status}`)
+    return response.status
+  }
+}
+
+export const addNewFavorite = (id, joke) => {
+  return fetch('http://localhost:3001/api/v1/favorites', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+       id: id,
+       joke: joke
+    }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
 }
