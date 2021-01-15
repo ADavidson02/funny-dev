b2fd2272bf6a163512a4d13afe8ba9cc807931a0
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-15 10:42:03 -0700

    Add condtional rendering for action button

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -2,11 +2,14 @@
 import React, { useEffect, useState } from 'react';
 import './Card.css';
 
-const Card = ({id, joke, saveCard}) => {
+const Card = ({id, joke, saveCard, deleteJoke }) => {
+  const pathname = window.location.pathname;
 
   return (
     <section className='joke-card'>
-      <button className='heart-button' onClick={() => saveCard(id)}>heart</button>
+      {pathname === '/' ?
+      <button className='heart-button' onClick={() => saveCard(id)}>heart</button> :
+      <button onClick={() => deleteJoke(id)}>trash</button>}
       <article key={id}>
         <h3>{joke}</h3>
       </article>
