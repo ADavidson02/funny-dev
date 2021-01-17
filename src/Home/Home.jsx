52d661c5bf6d889ff497f18181a83d97794d9302
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-16 20:42:16 -0700

    Remove unused code

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,7 @@
 
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import { getJoke, addNewFavorite } from "../apiCalls/apiCalls";
-import Card from '../Card/Card';
 import Container from '../Container/Container';
-import Saved from '../Saved/Saved';
 import { Link } from 'react-router-dom';
 import { Button } from "@material-ui/core";
 import './Home.css'
@@ -45,47 +43,4 @@ class Home extends Component {
   }
 }
 
-// function Home() {
-//   const [jokes, setJokes] = useState([])
-//   const [favorites, setFavorites] = useState([])
-//   const [error, setError] = useState('')
-
-//   const getJoke = async () => {
-//     const url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=3"
-//     setError('')
-
-//     try {
-//       const response = await fetch(url)
-//       const jokes = await response.json()
-//       setJokes(jokes)
-//       console.log(jokes)
-//     } catch(error) {
-//       setError(error.message)
-//     }
-//   }
-
-//   useEffect(() => {
-//     getJoke()
-//   }, [jokes])
-
-//   function saveCard(id) {
-//     console.log("click", id);
-//     const newFavorite = jokes.find((joke) => joke.id === id);
-//     if (!favorites.includes(newFavorite)) {
-//       setFavorites(newFavorite)
-//       // favorites.push(newFavorite);
-//       console.log(this.state.favorites);
-//     } else {
-//       return console.log("NO");
-//     }
-//   };
-
-//     return (
-//       <div>
-//         <h2>Home component</h2>
-//         <Container jokeSlips={jokes} saveCard={saveCard} />
-//         <Container jokeSlips={favorites} />
-//       </div>
-//     );
-// }
 export default Home;
