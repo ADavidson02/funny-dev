ef9672c276b74458f94dc33240c0a3fbac8a3e63
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-16 20:41:43 -0700

    Add prop types for card component

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,10 +1,11 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 import { Button, Checkbox, FormControlLabel } from '@material-ui/core';
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import DeleteIcon from "@material-ui/icons/Delete";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import PropTypes from 'prop-types';
 
 const Card = ({id, joke, saveCard, deleteJoke }) => {
   const pathname = window.location.pathname;
@@ -14,27 +15,38 @@ const Card = ({id, joke, saveCard, deleteJoke }) => {
     <section className="joke-card">
       {pathname === "/" ? (
         <FormControlLabel
-          control={<Checkbox
-          checked={checked}
-          onChange={(e) => setChecked(e.target.checked)}
-          inputProps={{'aria-label': 'save button'}}
-          icon={<FavoriteBorderIcon />}
-          checkedIcon={<FavoriteIcon />}
-          onClick={() => saveCard(id)}
-        />}
+          control={
+            <Checkbox
+              checked={checked}
+              onChange={(e) => setChecked(e.target.checked)}
+              inputProps={{ "aria-label": "save button" }}
+              icon={<FavoriteBorderIcon />}
+              checkedIcon={<FavoriteIcon />}
+              onClick={() => saveCard(id)}
+            />
+          }
           className="love-button"
         >
           save
-        </ FormControlLabel>
+        </FormControlLabel>
       ) : (
-        <Button startIcon={<DeleteIcon />} onClick={() => deleteJoke(id)}>
-        </Button>
+        <Button
+          startIcon={<DeleteIcon />}
+          onClick={() => deleteJoke(id)}
+        ></Button>
       )}
-      <article key={id}>
+      <article className="joke-card" key={id}>
         <h3>{joke}</h3>
       </article>
     </section>
   );
+}
+
+Card.propTypes = {
+  id: PropTypes.number,
+  joke: PropTypes.string,
+  saveCard: PropTypes.func,
+  deleteJoke: PropTypes.func
 }
 
 export default Card;
