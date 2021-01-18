a8c74d9e25ae763f9dc7aafd8de2e644a74209fd
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-17 17:23:07 -0700

    Add App component foulder

---
+++ src/App/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../Home/Home';
+import React from "react";
+import Saved from '../Saved/Saved';
+
+
+function App() {
+  return (
+    <section className="App">
+      <div>
+        <h2 className="App-header">Funny-Dev</h2>
+      </div>
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/favorites" component={Saved} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      </div>
+    </section>
+  );
+}
+
+export default App;
