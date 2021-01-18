9c1e83bcfff5ffa80ee6a3f1d514d922c9f96a8e
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-17 19:57:10 -0700

    Begin Card component testing

---
+++ src/Card/Card.test.js
@@ -0,0 +1,52 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import Card from './Card';
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+
+describe('Card', () => {
+  it('should render a jokeSlip', () => {
+    const mockJokeSlip = {
+      id: 26,
+      joke: '!False',
+      saveCard: jest.fn()
+    }
+    render(
+      <Card 
+      id={mockJokeSlip.id}
+      joke={mockJokeSlip.joke}
+      saveCard={mockJokeSlip.saveCard}
+      />
+      )
+      
+      expect(screen.getByText('!False')).toBeInTheDocument();
+      const saveButton = screen.getByRole("checkbox", { name: /save button/i })
+      expect(saveButton).toBeInTheDocument()
+    })
+    
+  // it('should render with a delete button', () => {
+  //   const history = createMemoryHistory();
+  //   const route = "/favorites";
+  //   history.push(route)
+  //   const mockJokeSlip2 = {
+  //     id: 26,
+  //     joke: '!True',
+  //     deleteJoke: jest.fn()
+  //   }
+
+  //   render(
+  //     <Router history={history}>
+  //     <Card 
+  //     id={mockJokeSlip2.id}
+  //     joke={mockJokeSlip2.joke}
+  //     deleteJoke={mockJokeSlip2.deleteJoke}
+  //     />
+  //     </Router>
+  //   )
+  //   screen.debug()
+  //   expect(screen.getByText("!True")).toBeInTheDocument();
+  //   const deleteButton = screen.getByRole("button", { name: /delete\-button/i })
+  //   expect(deleteButton).toBeInTheDocument
+  // })
+})
