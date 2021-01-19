3ef5e8ca42d131c21ed0b9a4d31d72683df92bbd
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-18 19:27:03 -0700

    Add test for search modal

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -86,5 +86,70 @@ describe("App", () => {
     const homeButton = screen.getByText('Home')
     userEvent.click(homeButton)
     await waitFor(() => expect(screen.getByText("Joke two")).toBeInTheDocument())
+  }),
+
+  it('should render a header', () => {
+    getAllFavorites.mockResolvedValue([
+      { id: 22, joke: "Joke twelve" },
+      { id: 24, joke: "Joke fourteen" },
+      { id: 26, joke: "Joke sixteen" },
+    ]);
+    const mockJokeSlips = [
+      { id: 68, joke: "Joke two" },
+      { id: 46, joke: "Joke one" },
+    ];
+
+    getJoke.mockResolvedValue({ jokes: mockJokeSlips });
+    render(
+      <App />
+    )
+    expect(screen.getByText("Funny-Dev")).toBeInTheDocument()
+  }),
+
+  it("should send user home when link is clicked", async () => {
+    const mockSaveJoke = jest.fn()
+    const saveCard = jest.fn()
+    const mockJokeSlips = [
+      { id: 68, joke: "Joke two" },
+      { id: 46, joke: "Joke one" }
+    ];
+
+    getJoke.mockResolvedValue({ jokes: mockJokeSlips });
+    getAllFavorites.mockResolvedValue([
+      { id: 22, joke: "Joke three" },
+      { id: 24, joke: "Joke four" },
+      { id: 26, joke: "Joke five" },
+    ]);
+    const history = createMemoryHistory()
+    await act( async () => {
+      renderWithRouter(
+      <Router history={history}>
+        <App />
+      </Router>,
+      { rout: "/"}
+      );
+    })
+
+    await waitFor(() =>
+      expect(screen.queryByText("Joke two")).toBeInTheDocument()
+    );
+
+    const favoritesLink = screen.getByText("My Favorites")
+    act(() => {
+      userEvent.click(favoritesLink)
+    })
+    await waitFor(() => 
+      expect(screen.getByText("Joke four")).toBeInTheDocument())
+    const searchModal = screen.getByText("Search");
+    userEvent.click(searchModal);
+    expect(screen.getByText("Search for a joke here")).toBeInTheDocument();
+    const searchButton = screen.getByTestId("search-button");
+    userEvent.type(screen.getByPlaceholderText("Search"), "two");
+    userEvent.click(searchButton);
+    expect(screen.getByTestId("search-error")).toBeInTheDocument();
+    const homeButton = screen.getByTestId("return-home")
+    userEvent.click(homeButton)
+    await waitFor(() => 
+      expect(screen.getByText("Joke one")).toBeInTheDocument())
   })
 })
