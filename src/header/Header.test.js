3551e0afbf2e62d867a66598bc0e9511e756a791
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-02-15 13:21:14 -0700

    Add test for header

--- src/header/Header.test.js
+++ src/header/Header.test.js
@@ -3,5 +3,10 @@ import '@testing-library/jest-dom'
 import Header from './Header.jsx'
 
 describe("Header", () => {
-  it('should render ')
+  it('should render the title of the site', () => {
+    render(
+      <Header />
+    )
+    expect("Funny-Dev").toBeInTheDocument()
+  })
 })
