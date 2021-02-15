08280a14b14cbdd44421e1238304bc7705f09ae8
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-02-15 15:24:13 -0700

    Change name of component

--- src/App/App.js
+++ src/App/App.js
@@ -4,13 +4,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import React from "react";
 import Saved from "../Saved/Saved";
-import Header from "../Header/Header";
+import Title from "../Title/Title"
 
 function App() {
   return (
     <section className="App">
       <div style={{marginTop: '8em'}}>
-        <Header />
+        <Title />
       </div>
       <div>
         <Router>
