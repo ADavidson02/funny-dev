932ecffd2a8105670a6546cd311401540238bdce
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-14 10:43:53 -0600

    Remove unneeded code

--- src/Title/Title.jsx
+++ src/Title/Title.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 import { AppBar } from '@material-ui/core'
 import Typography from '@material-ui/core/Typography'
 import Toolbar from '@material-ui/core/Toolbar'
-import { Button } from '@material-ui/core'
-
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 
 function Header() {
